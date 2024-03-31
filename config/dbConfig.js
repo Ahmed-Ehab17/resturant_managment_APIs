8a37f6837c60ca7da5cdc49ecbdfa03ceb0bacef
Karim-Elraay <karimsaiid12@gmail.com>
2024-03-31 23:19:24 +0200

    initial

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,30 +1,27 @@
 require("dotenv").config();
 const { Client } = require("pg");
-const fs = require('fs');
+const fs = require("fs");
 
 const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
-    connectionString : `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
     // Beware! The ssl object is overwritten when parsing the connectionString
-//     ssl: {
-//       rejectUnauthorized: false,
-//       ca: fs.readFileSync('./web server key.PEM').toString(),
-//     },
-  }
-const client = new Client(
-    config
-);
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync("./web server key.PEM").toString(),
+    },
+};
+const client = new Client(config);
 
-async function connect (){
+async function connect() {
     try {
         await client.connect();
-        console.log('Connected to PostgreSQL database!');
+        console.log("Connected to PostgreSQL database!");
     } catch (err) {
-        console.error('Connection error:', err.stack);
-    } 
-};
-
+        console.error("Connection error:", err.stack);
+    }
+}
 
 connect();
 
