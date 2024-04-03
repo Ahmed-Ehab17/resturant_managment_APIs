dfe83dd3b1022088dc4ee86aa11fb29ef4b18448
Karim-Elraay <karimsaiid12@gmail.com>
2024-04-03 22:16:19 +0200

    updated apis

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -1,16 +1,25 @@
+const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const branchController = require("../controllers/branchController");
 const { client } = require("../config/dbConfig");
 
-router.get('/add-new',(req, res)=>{
-    res.status(200).sendFile('add-new.html',{root: "../admin/Downloaded Web Sites/efood-admin.6amtech.com/admin/branch"});
-})
+router.get("/add-new", (req, res) => {
+    res.status(200).sendFile("add-new.html", {
+        root: "../admin/Downloaded Web Sites/efood-admin.6amtech.com/admin/branch",
+    });
+});
 
-router.post('/add-new',branchController.addNew);
+router.post("/add-new", branchController.addNew);
 
+router.get("/list", branchController.branchList);
 
-router.get('/list', branchController.branchList);
-  
+router.post("/add-storage", branchController.addStorage);
+
+router.post("add-menu-item", branchController.addMenuItem);
+
+router.post("add-ingredient", branchController.addIngredient);
+
+router.post("change-salary", branchController.changeSalary);
 
 module.exports = router;
