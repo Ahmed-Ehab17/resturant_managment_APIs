259bc62678d917c21a52b730f467510b7c1dd746
Ahmed Ehab <aehab5910@gmail.com>
2024-07-09 01:10:00 +0300

    return limiter again

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "development") {
     console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//Limit each IP to 100 requests per `window` (here, per 15 minutes)
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100,
