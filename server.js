9b766054a6886acbb33cdf2cc7b9d41efd9c5c99
Ahmed Ehab <aehab5910@gmail.com>
2024-07-09 00:56:30 +0300

    pause the limiter for testing

--- server.js
+++ server.js
@@ -21,15 +21,15 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
-    message:
-        'Too many accounts created from this IP, please try again after an hour',
-    });
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100,
+//     message:
+//         'Too many accounts created from this IP, please try again after an hour',
+//     });
 
-app.use('/admin', limiter);
-app.use('/user', limiter);
+// app.use('/admin', limiter);
+// app.use('/user', limiter);
 
 app.use("/admin/auth", require("./routes/authRoutes"));
 app.use("/admin/branch", require("./routes/branchRoutes"));
